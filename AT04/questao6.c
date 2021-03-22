ad32d84e975407d76e09fa5f79d921e9c22e1434
Victor Bohana <victorbohana@gmail.com>
2021-03-22 17:41:29 -0300

    at04 atualizacao

--- AT04/questao6.c
+++ AT04/questao6.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-char* ler3Letras(){
+char* ler3Letras(char a, char b, char c){
     static char letras[3];
-    for(int i = 0; i < 3; i++){
-        printf("Digite a #%d letra: ", i + 1);
-        scanf(" %c", &letras[i]);
-    }
+    letras[0] = a;
+    letras[1] = b;
+    letras[2] = c;
+
     return letras;
 }
 
 int main(){
-    char* resultado = ler3Letras();
+    char a, b, c;
+    printf("Digite 3 letras: ");
+    scanf("%c %c %c", &a, &b, &c);
+
+    char* resultado = ler3Letras(a, b, c);
     for(int i = 0; i < 3; i++){
         printf("%c, ", resultado[i]);
     }
