2f6f49c7a226658a40b7815b4e8007ccaa461a7d
Victor Bohana <victorbohana@gmail.com>
2021-03-24 17:51:48 -0300

    ultima atualizacao questao9

--- AT04/questao9.c
+++ AT04/questao9.c
@@ -3,7 +3,7 @@
 
 struct Cliente{
     char nome[21];
-    char dataNascimento[9];
+    char dataNascimento[11];
     char cpf[12];
     char sexo;
 };
@@ -24,11 +24,11 @@ void validarNome(char* nome){
 }
 
 void validarNascimento(char* nascimento){
-    for(int i = 0; i < 8; i++)
+    for(int i = 0; i < 11; i++)
     {
         if(nascimento[i] != '0' && nascimento[i] != '1' && nascimento[i] != '2' && nascimento[i] != '3' 
             && nascimento[i] != '4' && nascimento[i] != '5' && nascimento[i] != '6' && nascimento[i] != '7' 
-            && nascimento[i] != '8' && nascimento[i] != '9');
+            && nascimento[i] != '8' && nascimento[i] != '9' && nascimento[i] != '/' && nascimento[i] != '\0')
         {
             printf("Caractere invalido detectado na data.\n");
             exit(0);
@@ -53,24 +53,28 @@ void validarNascimento(char* nascimento){
             exit(0);
         }
     }
-    if(nascimento[2] != '0' && nascimento[2] != '1')
+    if(nascimento[3] != '0' && nascimento[3] != '1')
     {
         printf("Mes de nascimento invalido.\n");
         exit(0);
     }
-    if(nascimento[2] == '1'){
-        if(nascimento[3] != '0' && nascimento[3] != '1' && nascimento[3] != '2')
+    if(nascimento[3] == '1'){
+        if(nascimento[4] != '0' && nascimento[3] != '1' && nascimento[3] != '2')
         {
             printf("Mes de nascimento invalido.\n");
             exit(0);
         }
     }
-    if(nascimento[2] == '0'){
-        if(nascimento[3] == '0')
+    if(nascimento[3] == '0'){
+        if(nascimento[4] == '0')
         {
             printf("Mes de nascimento invalido.\n");
             exit(0);
         }
+    }
+    if(nascimento[2] != '/' && nascimento[5] != '/'){
+        printf("Formatacao da data incorreta.\n");
+        exit(0);
     }
 }
 
@@ -79,7 +83,7 @@ void validarCPF(char* cpf){
     {
         if(cpf[i] != '0' && cpf[i] != '1' && cpf[i] != '2' && cpf[i] != '3' 
         && cpf[i] != '4' && cpf[i] != '5' && cpf[i] != '6' && cpf[i] != '7' 
-        && cpf[i] != '8' && cpf[i] != '9')
+        && cpf[i] != '8' && cpf[i] != '9' && cpf[i] != '\0')
         {
             printf("Caractere invalido detectado no CPF.\n");
             exit(0);
@@ -102,13 +106,12 @@ struct Cliente cadastrarCliente(char* nome, char* nascimento, char* cpf, char se
     validarCPF(cpf);
     validarSexo(sexo);
 
-
     for(int i = 0; i < 21; i++)
     {
         cliente.nome[i] = nome[i];
     }
 
-    for(int j = 0; j < 9; j++)
+    for(int j = 0; j < 11; j++)
     {
         cliente.dataNascimento[j] = nascimento[j];
     }
@@ -124,11 +127,11 @@ struct Cliente cadastrarCliente(char* nome, char* nascimento, char* cpf, char se
 }
 
 int main(){
-    char nome[21], nascimento[9], cpf[12], sexo;
+    char nome[21], nascimento[11], cpf[12], sexo;
     printf("Digite o nome do cliente: ");
     scanf(" %s", nome);
 
-    printf("Digite a data de nascimento do cliente: ");
+    printf("Digite a data de nascimento do cliente(DD/MM/YYYY): ");
     scanf(" %s", nascimento);
 
     printf("Digite o CPF do cliente (XXXXXXXXXXX): ");
