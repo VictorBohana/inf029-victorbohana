1a5956aada02958437b237818b90ec9451dd1da7
Victor Bohana <victorbohana@gmail.com>
2021-06-01 21:44:59 -0300

    corrigido

--- Trabalho1/corretor.c
+++ Trabalho1/corretor.c
@@ -16,7 +16,8 @@
 
 #include <stdio.h>
 #include <wctype.h>
-#include<stddef.h>
+#include <wchar.h>
+#include <stddef.h>
 #include <string.h>
 #include <locale.h>
 
@@ -37,12 +38,18 @@ int main()
     setlocale(LC_ALL, "pt_BR.UTF-8");
     //testSomar();
     //testFatorial();
-    //testQ1();
-    //testQ2();
-    //testQ3();
+    wprintf(L"TESTE Q1\n");
+    testQ1();
+    wprintf(L"TESTE Q2\n");
+    testQ2();
+    wprintf(L"TESTE Q3\n");
+    testQ3();
+    wprintf(L"TESTE Q4\n");
     testQ4();
-    //testQ5();
-    //testQ6();
+    wprintf(L"TESTE Q5\n");
+    testQ5();
+    wprintf(L"TESTE Q6\n");
+    testQ6();
 }
 
 void testSomar()
@@ -65,15 +72,15 @@ void testQ1()
 {
     char str[11];
     strcpy(str, "29/02/2015");
-    printf("%d\n", q1(str) == 0);
+    wprintf(L"%d\n", q1(str) == 0);
     strcpy(str, "29/02/2012");
-    printf("%d\n", q1(str) == 1);
+    wprintf(L"%d\n", q1(str) == 1);
     strcpy(str, "9/13/2014");
-    printf("%d\n", q1(str) == 0);
+    wprintf(L"%d\n", q1(str) == 0);
     strcpy(str, "45/4/2014");
-    printf("%d\n", q1(str) == 0);
+    wprintf(L"%d\n", q1(str) == 0);
     strcpy(str, "/9/2014");
-    printf("%d\n", q1(str) == 0);
+    wprintf(L"%d\n", q1(str) == 0);
 
 }
 
@@ -91,10 +98,10 @@ void testQ2()
     strcpy(datainicial, "01/06/2015");
     strcpy(datafinal, "01/06/2016");
     retorno = q2(datainicial, datafinal, &qtdDias, &qtdMeses, &qtdAnos);
-    printf("%d\n", retorno == 1);
-    printf("%d\n", qtdDias == 0);
-    printf("%d\n", qtdMeses == 0);
-    printf("%d\n", qtdAnos == 1);
+    wprintf(L"%d\n", retorno == 1);
+    wprintf(L"%d\n", qtdDias == 0);
+    wprintf(L"%d\n", qtdMeses == 0);
+    wprintf(L"%d\n", qtdAnos == 1);
 
     //teste 2 - retornos
     qtdDias = -1;
@@ -104,17 +111,17 @@ void testQ2()
     strcpy(datainicial, "01/30/2015");
     strcpy(datafinal, "01/06/2016");
     retorno = q2(datainicial, datafinal, &qtdDias, &qtdMeses, &qtdAnos);
-    printf("%d\n", retorno == 2);
+    wprintf(L"%d\n", retorno == 2);
 
     strcpy(datainicial, "01/3/2015");
     strcpy(datafinal, "40/06/2016");
     retorno = q2(datainicial, datafinal, &qtdDias, &qtdMeses, &qtdAnos);
-    printf("%d\n", retorno == 3);
+    wprintf(L"%d\n", retorno == 3);
 
     strcpy(datainicial, "01/06/2016");
     strcpy(datafinal, "01/06/2015");
     retorno = q2(datainicial, datafinal, &qtdDias, &qtdMeses, &qtdAnos);
-    printf("%d\n", retorno == 4);
+    wprintf(L"%d\n", retorno == 4);
 
     //teste 3
     qtdDias = -1;
@@ -124,10 +131,10 @@ void testQ2()
     strcpy(datainicial, "06/06/2017");
     strcpy(datafinal, "07/07/2017");
     retorno = q2(datainicial, datafinal, &qtdDias, &qtdMeses, &qtdAnos);
-    printf("%d\n", retorno == 1);
-    printf("%d\n", qtdDias == 1);
-    printf("%d\n", qtdMeses == 1);
-    printf("%d\n", qtdAnos == 0);
+    wprintf(L"%d\n", retorno == 1);
+    wprintf(L"%d\n", qtdDias == 1);
+    wprintf(L"%d\n", qtdMeses == 1);
+    wprintf(L"%d\n", qtdAnos == 0);
 }
 
 void testQ3()
@@ -176,22 +183,22 @@ void testQ4()
 
 void testQ5()
 {
-    printf("%d\n", q5(345) == 543);
-    printf("%d\n", q5(78) == 87);
-    printf("%d\n", q5(3) == 3);
-    printf("%d\n", q5(5430) == 345);
-    printf("%d\n", q5(2135) == 5312);
-    printf("%d\n", q5(6966) == 6696);
+    wprintf(L"%d\n", q5(345) == 543);
+    wprintf(L"%d\n", q5(78) == 87);
+    wprintf(L"%d\n", q5(3) == 3);
+    wprintf(L"%d\n", q5(5430) == 345);
+    wprintf(L"%d\n", q5(2135) == 5312);
+    wprintf(L"%d\n", q5(6966) == 6696);
 }
 
 void testQ6()
 {
-    printf("%d\n", q6(34567368, 3) == 2);
-    printf("%d\n", q6(34567368, 4576) == 0);
-    printf("%d\n", q6(3539343, 3) == 4);
-    printf("%d\n", q6(3539343, 39) == 1);
-    printf("%d\n", q6(5444, 44) == 1);
-    printf("%d\n", q6(54444, 44) == 2);
-    printf("%d\n", q6(3434, 34) == 2);
-    printf("%d\n", q6(123456235, 23) == 2);
+    wprintf(L"%d\n", q6(34567368, 3) == 2);
+    wprintf(L"%d\n", q6(34567368, 4576) == 0);
+    wprintf(L"%d\n", q6(3539343, 3) == 4);
+    wprintf(L"%d\n", q6(3539343, 39) == 1);
+    wprintf(L"%d\n", q6(5444, 44) == 1);
+    wprintf(L"%d\n", q6(54444, 44) == 2);
+    wprintf(L"%d\n", q6(3434, 34) == 2);
+    wprintf(L"%d\n", q6(123456235, 23) == 2);
 }
